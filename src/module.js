bb7584fa0afb41baa14ca2eaf0347890681f841e
sameeram <mgjaye@gmail.com>
2016-09-23 14:49:10 +0530

    Fixed component path

--- src/module.js
+++ src/module.js
@@ -1,5 +1,5 @@
-import { MyEditorPageCtrl } from 'components/myeditor_page_ctrl.js';
-import { MyViewPageCtrl } from 'components/myview_page_ctrl.js';
+import { MyEditorPageCtrl } from '/components/myeditor_page_ctrl.js';
+import { MyViewPageCtrl } from '/components/myview_page_ctrl.js';
 
 export {
 	MyEditorPageCtrl,
